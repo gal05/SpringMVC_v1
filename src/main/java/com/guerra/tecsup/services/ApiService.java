461956442f97f2c6d6f4f0d74b6ca737879d910a
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-05-21 10:14:47 -0500

    21-05-18 ok!

--- src/main/java/com/guerra/tecsup/services/ApiService.java
+++ src/main/java/com/guerra/tecsup/services/ApiService.java
@@ -2,22 +2,34 @@ package com.guerra.tecsup.services;
 
 import java.util.List;
 
+import com.guerra.tecsup.model.Github;
 import com.guerra.tecsup.model.Login;
 import com.guerra.tecsup.model.Sede;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ApiService {
 	
 		
-    String API_BASE_URL = "https://pi4-v2-larav-pass-excel-api-gal05.c9users.io";
+    //String API_BASE_URL = "https://pi4-v2-larav-pass-excel-api-gal05.c9users.io";
+	String API_BASE_URL = "https://api.github.com";
 
     @GET("api/sede")
     Call<List<Sede>> getSedes();
     
     
     
+    
+    @GET("/users")
+    public Call<List<Github>> getUsers(
+      @Query("per_page") int per_page, 
+      @Query("page") int page);
+ 
+    @GET("/users/{username}")
+    public Call<Github> getUser(@Path("username") String username);
     
 
     
