e48a2ea8c5e00a05f480cda0825524bb252a577c
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-05-20 23:04:47 -0500

    funcionamiento OK!-  implementando login en spring

---
+++ src/main/java/com/guerra/tecsup/controller/InicioController.java
@@ -0,0 +1,20 @@
+package com.guerra.tecsup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.guerra.tecsup.model.Empleado;
+
+
+@Controller
+public class InicioController {
+
+	
+	
+	/*@GetMapping(value={"/juu", "login"})
+	public ModelAndView preLogin() {
+		Empleado emp = new Empleado();
+		return new ModelAndView("login", "command", emp);
+	}*/
+}
