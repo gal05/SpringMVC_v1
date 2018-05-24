edbf10f94ffb88b863f5c0f4ca30d6aab0a1d919
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-05-24 01:45:13 -0500

    Login y listar DonE!!!!

--- src/main/java/com/guerra/tecsup/controller/InicioController.java
+++ src/main/java/com/guerra/tecsup/controller/InicioController.java
@@ -1,5 +1,7 @@
 package com.guerra.tecsup.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -10,23 +12,78 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.guerra.tecsup.model.Empleado;
 import com.guerra.tecsup.model.Sede;
+import com.guerra.tecsup.services.ApiService;
+import com.guerra.tecsup.services.ApiServiceGenerator;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Call;
 
 
 @Controller
 public class InicioController {
 
-	
+	List<Sede>sedes=null;
 	private static final Logger logger = LoggerFactory.getLogger(InicioController.class);	
 	@GetMapping("/to/menu")
 	public String list(@ModelAttribute("SpringWeb") Sede sedes, ModelMap model) {
-
-		try {
-			//model.addAttribute("employees", employeeService.findAll());
-		} catch (Exception e) {
-			logger.info(e.getMessage());
-			model.addAttribute("message", e.getMessage());
-		}
+		
+		model.addAttribute("sedes", listarSedes());
+		
 
 		return "menu";
 	}
+	
+
+	private List<Sede> listarSedes()
+	{
+		
+		ApiService service = ApiServiceGenerator.createService(ApiService.class);
+
+        Call<List<Sede>> call = service.getSedes();
+
+        call.enqueue(new Callback<List<Sede>>() {
+            @Override
+            public void onResponse(Call<List<Sede>> call, Response<List<Sede>> response) {
+                try {
+
+                    int statusCode = response.code();
+                    System.out.println("HTTP status code: " + statusCode);
+
+                    if (response.isSuccessful()) {
+
+                        sedes = response.body();
+                        System.out.println("productos: " + sedes);
+
+                    } else {
+                        System.out.println("onError: " + response.errorBody().string());
+                        throw new Exception("Error en el servicio");
+                    }
+
+                } catch (Throwable t) {
+                    try {
+                        System.out.println("onThrowable: " + t.toString());
+                        System.out.println(t.getMessage());
+                    }catch (Throwable x){}
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Sede>> call, Throwable t) {
+            	System.out.println(t.toString());
+                System.out.println( t.getMessage());
+            }
+
+        });
+	
+		return  sedes;
+	}	
+	
 }
+
+
+
+
+
+
+
+
--- src/main/webapp/WEB-INF/views/menu.jsp
+++ src/main/webapp/WEB-INF/views/menu.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,7 +10,47 @@
 </head>
 <body>
 <h1>funciona!!</h1>
-<h2><font color="red">Mensaje : ${login}</font></h2>
+<table class="table table-bordered table-striped">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>nombre</th>
+							<th>ubicacion</th>
+							<!--  th>Dept</th -->
+							<th>Edit</th>
+							<th>Delete</th>
+						</tr>
+					</thead>
+					<tbody>
+						<c:forEach var="sede" items="${sedes}">
+							<tr>
+								<td>${sede.id}</td>
+								<td>${sede.nombre}</td>
+								<td>${sede.ubicacion}</td>
+								<td><a
+									href="<%=request.getContextPath()%>/admin/emp/editform/${sede.id}"
+									class="btn btn-info btn-xs"> <i
+										class="glyphicon glyphicon-check"></i>Edit
+								</a></td>
+								<td><a
+									href="<%=request.getContextPath()%>/admin/emp/deleteform/${sede.id}"
+									class="btn btn-danger btn-xs"> <i
+										class="glyphicon glyphicon-trash"></i> Delete
+								</a></td>
+							</tr>
+						</c:forEach>
+					</tbody>
+				</table>
+				
+				
+				<div>
+				
+				</div>
+
+
+
+
+
 
 </body>
 </html>
