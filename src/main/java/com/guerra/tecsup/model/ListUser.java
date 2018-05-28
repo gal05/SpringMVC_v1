d9d6f23ba0d075e25d8f347e93a89887dacb943a
Jose Guerra <jose.guerra.g@tecsup.edu.pe>
2018-05-27 19:40:00 -0500

    :v faltaron estos

---
+++ src/main/java/com/guerra/tecsup/model/ListUser.java
@@ -0,0 +1,15 @@
+package com.guerra.tecsup.model;
+
+public class ListUser {
+	
+	private int id;
+	private String email;
+	private String password;
+	private String name;
+	private String codigo;
+	private int tipo;
+
+	
+	
+	
+}
